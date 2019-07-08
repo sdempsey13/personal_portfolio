23f90124a5cc46cfd7029559f21a717c1b3291bb
sdempsey13 <stevendempsey13@gmail.com>
2019-07-07 20:59:34 -0400

    Fixed formatting current_user_concerns.rb.

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -7,8 +7,8 @@ module CurrentUserConcern
 
   def guest_user
     OpenStruct.new(:name => "Guest User",
-                            :first_name => "Guest",
-                            :last_name => "User",
-                            :email => "guest@example.com")
+                   :first_name => "Guest",
+                   :last_name => "User",
+                   :email => "guest@example.com")
   end
 end
