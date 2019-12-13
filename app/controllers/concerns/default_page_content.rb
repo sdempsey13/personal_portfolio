547467ca8d8e6540b437fd31200b29183914e093
sdempsey13 <stevendempsey13@gmail.com>
2019-12-13 10:43:42 -0800

    updated nav bar and changed page title.

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "Steven Dempsey | My Portfolio Website"
     @seo_keywords = "Steven Dempsey portfolio"
   end
 end
